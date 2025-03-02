b42b421fed5c0f09a4310a64f05c9a31fe526102
DamianCU <158050732+DamianCU@users.noreply.github.com>
2025-03-02 20:57:45 +0100

    Java Pacman

--- src/PacMan.java
+++ src/PacMan.java
@@ -32,5 +32,11 @@ public class PacMan extends JPanel{
         blueGhostImage = new ImageIcon(getClass().getResource("./img/blueGhost.png")).getImage();
         orangeGhostImage = new ImageIcon(getClass().getResource("./img/orangeGhost.png")).getImage();
         pinkGhostImage = new ImageIcon(getClass().getResource("./img/pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./img/redGhost.png")).getImage();
+
+        pacmanUpImage = new ImageIcon(getClass().getResource("./img/pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("./img/pacmanDown.png")).getImage();
+        pacmanLeftImage = new ImageIcon(getClass().getResource("./img/pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("./img/pacmanRight.png")).getImage();
     }
 }
